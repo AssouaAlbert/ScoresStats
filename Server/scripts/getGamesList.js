54ff0958d7252935f302505beeff81a3acb64249
Albert Assoua <assouaalbert@gmail.com>
2023-07-12 13:18:43 -0400

    On branch master Changes to be committed: modified:   Client/src/App.js modified:   Server/package-lock.json modified:   Server/package.json modified:   Server/scripts/getGamesList.js

--- Server/scripts/getGamesList.js
+++ Server/scripts/getGamesList.js
@@ -1,10 +1,24 @@
 const puppeteer = require("puppeteer");
-
 const scrollPageGetLinks = require("./scrollPageGetLinks.js");
 const checkIfLeague = require("./checkIfLeague.js");
 
+require("dotenv").config();
+
 const getGamesList = async () => {
-  const browser = await puppeteer.launch({ headless: true, dumpio: false });
+  const browser = await puppeteer.launch({
+    headless: true,
+    dumpio: false,
+    args: [
+      "--no-sandbox",
+      "---disable-setuid-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
   await page.setViewport({
     width: 1200,
@@ -12,10 +26,10 @@ const getGamesList = async () => {
   });
   await page.goto("https://www.livescores.com/?tz=-4");
   let gamesList = await page
-    .waitForSelector('#content-center')
+    .waitForSelector("#content-center")
     .then(async () => {
       const gamesLinks = await scrollPageGetLinks(page);
-      return gamesLinks
+      return gamesLinks;
     });
   page.close();
   newPage = await browser.newPage();
